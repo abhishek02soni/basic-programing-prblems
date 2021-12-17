e654dcc350c279eea8a5bc650f88335dbde376c6
Abhishek <abhisheksoni9421@gmail.com>
2021-12-17 08:52:45 +0530

    this is a even number program

---
+++ src/evenNumbers/EvenNumbers.java
@@ -0,0 +1,19 @@
+package evenNumbers;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int arr[] = { 1,2,3,4,5,6};
+     int temp=0;
+      for(int i=0; i<arr.length;i++) {
+       
+    	if(arr[i]%2==0) {
+      
+      System.out.println(arr[i]);
+    	}
+     
+      }  
+	}
+}
+
